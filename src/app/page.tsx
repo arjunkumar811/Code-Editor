8dd841d5630ef23eb7551ed51d6f07c975179014
arjunkumar811 <kumararjun26401@gmail.com>
2025-02-16 18:05:12 +0530

    Added Auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,25 @@
+import { SignedIn, SignedOut, SignOutButton, SignUpButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
-      hi there
+      <SignedOut>
+ <SignUpButton>
+  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+  Sign Up
+  </button>
+ </SignUpButton>
+      </SignedOut>
+
+<UserButton />
+
+      <SignedIn>
+  <SignOutButton>
+  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+    Sign Out
+     </button>
+  </SignOutButton>
+      </SignedIn>
     </div>
   );
 }

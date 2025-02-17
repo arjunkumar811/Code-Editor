371e2996d0af0011f453c6084a178b9d7e34a460
arjunkumar811 <kumararjun26401@gmail.com>
2025-02-18 01:28:44 +0530

    Added DB schema

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    // <ClerkProvider>
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
@@ -37,6 +37,6 @@ export default function RootLayout({
         
       </body>
     </html>
-    </ClerkProvider>
+    // </ClerkProvider>
   );
 }

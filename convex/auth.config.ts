094ff6602a17962d95919966c5e8966b8c3b860f
arjunkumar811 <kumararjun26401@gmail.com>
2025-02-25 23:09:36 +0530

    Added clerk-provider

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,8 +1,10 @@
 export default {
     providers: [
       {
-        domain: "https://sharing-quetzal-51.clerk.accounts.dev",
+        domain: "https://sharing-quetzal-51.clerk.accounts.dev/",
         applicationID: "convex",
       },
     ]
   };
+
+
--- convex/users.ts
+++ convex/users.ts
@@ -26,3 +26,4 @@ export const syncUser = mutation({
     },
 })
 
+
